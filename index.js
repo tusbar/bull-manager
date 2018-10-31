faee28f72567fb8a7679fed193385d4134331755
Bertrand Marron <bertrand.marron@gmail.com>
2018-10-31 18:23:14 +0100

    Initialize repository

---
+++ index.js
@@ -0,0 +1,126 @@
+const Queue = require('bull')
+const _ = require('lodash')
+
+let queues = {}
+let globalOptions = {
+  isSubscriber: false
+}
+let client = null
+let subscriber = null
+
+function configureQueues(options) {
+  globalOptions = {
+    ...globalOptions,
+    ...options
+  }
+
+  if (globalOptions.createRedis) {
+    client = globalOptions.createRedis()
+
+    if (globalOptions.isSubscriber) {
+      subscriber = globalOptions.createRedis()
+    }
+  }
+}
+
+const _createQueue = (name, jobOptions) => {
+  jobOptions = {
+    removeOnComplete: true,
+    ...jobOptions
+  }
+
+  const queue = new Queue(name, {
+    createClient: globalOptions.createRedis ? type => {
+      switch (type) {
+        case 'client':
+          return client
+        case 'subscriber':
+          if (!globalOptions.isSubscriber) {
+            throw new Error('Subscriber queue was needed in client mode')
+          }
+          return subscriber
+        default:
+          return globalOptions.createRedis()
+      }
+    } : undefined,
+    prefix: globalOptions.prefix
+  })
+
+  queues[name] = {
+    queue,
+    jobOptions
+  }
+
+  return queue
+}
+
+async function createJobQueue(name, handler, options, jobOptions) {
+  if (!globalOptions.isSubscriber) {
+    throw new Error('Cannot create a job queue in client mode')
+  }
+
+  if (name in queues) {
+    throw new Error(`Job queue "${name}" already exists`)
+  }
+
+  const queue = _createQueue(name, jobOptions)
+
+  options = {
+    concurrency: 1,
+    onError: globalOptions.onError,
+    options
+  }
+
+  queue.process(options.concurrency, handler)
+
+  if (options.onError) {
+    queue.on('failed', options.onError)
+  }
+
+  await queue.isReady()
+  return queue
+}
+
+async function joinJobQueue(name, jobOptions) {
+  const queue = _createQueue(name, jobOptions)
+
+  await queue.isReady()
+  return queue
+}
+
+function enqueue(name, data, options) {
+  if (!(name in queues)) {
+    throw new Error(`Job queue "${name}" does not exist`)
+  }
+
+  const {
+    queue,
+    jobOptions: {jobIdKey, ...jobOptions}
+  } = queues[name]
+
+  options = {
+    ...jobOptions,
+    jobId: jobIdKey ? _.get(data, jobIdKey) : undefined,
+    ...options
+  }
+
+  return queue.add(data, options)
+}
+
+async function disconnectQueues() {
+  await Promise.all(
+    queues.map(({queue}) => queue.disconnect())
+  )
+
+  queues = {}
+  client = null
+  subscriber = null
+}
+
+module.exports = {
+  configureQueues,
+  createJobQueue,
+  joinJobQueue,
+  enqueue,
+  disconnectQueues
+}
---
+++ readme.md
@@ -0,0 +1,95 @@
+# bull-manager [![CircleCI](https://circleci.com/gh/tusbar/bull-manager.svg?style=svg)](https://circleci.com/gh/tusbar/bull-manager)
+
+> Easily manage your bull queues
+
+[![npm version](https://badgen.net/npm/v/@tusbar/bull-manager)](https://www.npmjs.com/package/bull-manager)
+[![dependencies Status](https://badgen.net/david/dep/tusbar/bull-manager)](https://david-dm.org/tusbar/bull-manager)
+[![codecov](https://badgen.net/codecov/c/github/tusbar/bull-manager)](https://codecov.io/gh/tusbar/bull-manager)
+[![XO code style](https://badgen.net/badge/code%20style/XO/cyan)](https://github.com/xojs/xo)
+
+## Getting started
+
+```bash
+$ yarn add bull-manager
+```
+
+## Usage
+
+On the worker side:
+
+```js
+const {configureQueues, createJobQueue} = require('bull-manager')
+
+configureQueues({
+  isSubscriber: true,
+  prefix: 'my-prefix',
+  createRedis: type => {
+    /* … */
+  },
+  onError: (job, error) => {
+    console.error('An unexpected error happened')
+  }
+})
+
+await createJobQueue('my-cool-job', job => {/* handler */}, {
+  concurrency: 10,
+  onError: (job, error) => {
+    console.log('my cool job failed')
+  }
+}, {
+  /* job options */
+})
+```
+
+On the client side:
+
+```js
+const {configureQueues, joinJobQueue, enqueue} = require('bull-manager')
+
+configureQueues({
+  prefix: 'my-prefix',
+  createRedis: type => {
+    /* … */
+  },
+  onError: (job, error) => {
+    console.error('An unexpected error happened')
+  }
+})
+
+await joinJobQueue('my-cool-job', {
+  /* job options */
+})
+
+await enqueue('my-cool-job', {
+  something: 'important'
+}, {
+  /* job options */
+})
+```
+
+## Cool features
+
+### `jobIdKey`
+
+Pass `jobIdKey` to job options so that `enqueue` will set the `jobId` to `_.get(data, jobIdKey)`.
+
+## Random things
+
+- You don’t need to pass job options to `createJobQueue` if you are never going to use `enqueue` in your workers.
+
+## License
+
+MIT
+
+
+## Miscellaneous
+
+```
+    ╚⊙ ⊙╝
+  ╚═(███)═╝
+ ╚═(███)═╝
+╚═(███)═╝
+ ╚═(███)═╝
+  ╚═(███)═╝
+   ╚═(███)═╝
+```
